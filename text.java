495f30e52e30b432169f6c772932c57ad01ac5bd
Amananwr <72744902+Amananwr@users.noreply.github.com>
2020-11-08 21:56:00 +0800

    Add files via upload

---
+++ text.java
@@ -0,0 +1,13 @@
+package choose;
+
+public class text{
+	public static void main(String choose[]){
+		//import choose.people;
+		//import choose.teacher;
+		//import choose.student;
+		//import choose.course;
+		student stu1 = new student("1","刘泽","男","美术");
+		teacher tea1 = new teacher("1","徐红敏","女","线代");
+	}
+}
+
